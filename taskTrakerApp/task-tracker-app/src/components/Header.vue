e64b839ff10d859f14bdd47ceba3d4ff5a6e6bfc
sahiljassal <sahiljassal9198@gmail.com>
2024-01-31 00:43:09 +0530

    vue cli and componenets and props

---
+++ taskTrakerApp/task-tracker-app/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <MyCustomButton />
+  </header>
+</template>
+
+<script>
+import MyCustomButton from './Button.vue'
+
+export default {
+  name: 'my-header',
+  components: {
+    MyCustomButton
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'Hello World'
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
